a8ed5ca77a884b78b66e213da31c604fbfb6c105
n-ayako <ayako.nakano@fonfun.co.jp>
2024-05-13 09:45:41 +0900

    ユーザ登録用のsave情報

---
+++ src/main/java/com/example/portfolio/dao/UsersMapper.java
@@ -0,0 +1,17 @@
+package com.example.portfolio.dao;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import com.example.portfolio.dto.UserAddRequest;
+import com.example.portfolio.entity.Users;
+
+@Mapper
+public interface UsersMapper {
+	
+    /**
+     * ユーザー情報登録
+     * @param userRequest 登録用リクエストデータ
+     */
+    void save(UserAddRequest userRequest);
+	
+}
