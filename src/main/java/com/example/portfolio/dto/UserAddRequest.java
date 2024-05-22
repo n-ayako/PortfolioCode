f9b4c610d685bf4798bdb23e96ae936bb03909a5
n-ayako <ayako.nakano@fonfun.co.jp>
2024-05-23 07:39:46 +0900

    不要なインポート削除

--- src/main/java/com/example/portfolio/dto/UserAddRequest.java
+++ src/main/java/com/example/portfolio/dto/UserAddRequest.java
@@ -6,7 +6,6 @@ import lombok.Data;//アノテーションを付けるとetter, setter, toString
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 
 //ユーザー情報登録 リクエストデータ
