be145a9153fb6a280870b2aa05cffa6ab5441645
n-ayako <ayako.nakano@fonfun.co.jp>
2024-05-30 10:33:34 +0900

    表示テスト

--- src/main/java/com/example/portfolio/entity/Categories.java
+++ src/main/java/com/example/portfolio/entity/Categories.java
@@ -3,6 +3,8 @@ package com.example.portfolio.entity;
 import java.io.Serializable;
 import lombok.Data;
 import java.util.Date;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -16,4 +18,5 @@ public class Categories implements Serializable {
     private String name;
     private Date created_at;
     private Date ted_at;
+    private List<Categories> categoriesList;
 }
