d265eebe80de2a8d2916c02ef60c1c23e0752783
n-ayako <ayako.nakano@fonfun.co.jp>
2024-06-04 10:03:51 +0900

    studyTimeをIntegerに変更

--- src/main/java/com/example/portfolio/dto/UserSkillNew.java
+++ src/main/java/com/example/portfolio/dto/UserSkillNew.java
@@ -1,5 +1,6 @@
 package com.example.portfolio.dto;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 import jakarta.validation.constraints.Min;
@@ -15,15 +16,15 @@ public class UserSkillNew {
     private Integer categoryId;
     private String categoryName;
     private Long userId;
-	private Date month;
+	private LocalDate month;
 
 	@NotEmpty(message = "項目名は必ず入力してください")
     @Size(max = 50,message ="項目名は50文字以内で入力してください")
     private String learningDataName;
 
-    @NotNull(message = "学習時間は必ず入力してください")
+	@NotNull(message = "学習時間は必ず入力してください")
     @Min(value = 1, message = "学習時間は1以上の数字で入力してください")
-    private int studyTime;
+    private Integer studyTime;
 
     // ゲッターとセッター
     public Long getId() {
@@ -66,18 +67,18 @@ public class UserSkillNew {
         this.learningDataName = learningDataName;
     }
 
-    public int getStudyTime() {
+    public Integer getStudyTime() {
         return studyTime;
     }
 
-    public void setStudyTime(int studyTime) {
+    public void setStudyTime(Integer studyTime) {
         this.studyTime = studyTime;
     }
-    public Date getMonth() {
+    public LocalDate getMonth() {
         return month;
     }
 
-    public void setMonth(Date month) {
-        this.month = month;
+    public void setMonth(LocalDate currentDate) {
+        this.month = currentDate;
     }
 }
