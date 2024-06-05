6394d7d232d2944868ea8e2c40b3e268acf50d51
n-ayako <ayako.nakano@fonfun.co.jp>
2024-06-05 13:52:27 +0900

    lombok.Dataと@Dataを導入したため、ゲッターとセッターを削除

--- src/main/java/com/example/portfolio/dto/UserSkillEdit.java
+++ src/main/java/com/example/portfolio/dto/UserSkillEdit.java
@@ -1,71 +1,18 @@
 package com.example.portfolio.dto;
 
-import java.io.Serializable;
-import java.time.LocalDate;
-import java.util.Date;
-import java.util.Locale.Category;
+import java.sql.Date;
 
 import lombok.Data;
 
-
+@Data
 public class UserSkillEdit {
-
-	private long id;
-	private int categoryId;
-    private String categoryName;
-    private String learningDataName;
-    private int studyTime;
-	private Date month;
-    
-    // ゲッターとセッター
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long userId) {
-        this.id = userId;
-    }
-    
-    public int getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public String getLearningDataName() {
-        return learningDataName;
-    }
-
-    public void setLearningDataName(String learningDataName) {
-        this.learningDataName = learningDataName;
-    }
-    
-    public int getStudyTime() {
-        return studyTime;
-    }
-
-    public void setStudyTime(int studyTime) {
-        this.studyTime = studyTime;
-    }
-    
-    public Date getMonth() {
-        return month;
-    }
-
-    public void setMonth(Date month) {
-        this.month = month;
-    }
-
+    private long learningDataId;  // learning_dataのID
+    private long userId;          // ユーザーのID
+    private int categoryId;       // カテゴリのID
+    private String categoryName;  // カテゴリの名前
+    private String learningDataName; // 学習データの名前
+    private int studyTime;        // 学習時間
+    private Date month;           // 月
 }
 
 
